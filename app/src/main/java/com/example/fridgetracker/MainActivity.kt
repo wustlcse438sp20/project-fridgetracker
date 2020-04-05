22bf2f0028841e3f358d37f60aef75a3325dcd8f
Amy Kim <amy.kim@wustl.edu>
2020-04-04 22:18:10 -0500

    test commit

--- app/src/main/java/com/example/fridgetracker/MainActivity.kt
+++ app/src/main/java/com/example/fridgetracker/MainActivity.kt
@@ -9,7 +9,7 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+ 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
